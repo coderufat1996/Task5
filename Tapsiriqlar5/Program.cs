80cfdf3db778d3fd24e0e12b7b1ad451d275cf50
coderufat1996 <rufat.karimli96@mail.ru>
2023-03-25 13:58:36 +0400

    Add project files.

---
+++ Tapsiriqlar5/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Tapsiriqlar5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 7;
+            int b = 10;
+        }
+    }
+}
